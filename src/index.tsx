6a937ea75bdb7a3dd1f989e980d2b6d24ef651f3
VladislavGashnikov <MonsterLPchanel@gmail.com>
2024-01-29 02:12:51 +0300

    change base

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import '@/styles/index.scss';
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import AppRouter from './router/AppRouter';
 
 const root = document.getElementById('root') as HTMLElement;
 
 createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppRouter />
-  </BrowserRouter>,
+  </HashRouter>,
 );
